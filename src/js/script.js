0f3a7d82a738af56eb41744dfdd3b4c5d3ea696d
Giovanna <giosay@hotail.com>
2025-04-18 17:51:08 -0300

    Adicionando demonstração da saída da variável

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,12 @@
 
 //Variável com valor undefined
 let fruta;
-console.log("A fruta se chama": fruta);
+console.log("A fruta se chama": fruta);  //Saída como: undefined
 
 //Variável com valor vazio
 let cor = null;
-console.log("A cor escolhida é": cor);
+console.log("A cor escolhida é": cor);  //Saída como: string vazia
 
 //Variável com valor
 let idade = 18;
-console.log("A idade é igual a:" idade)
+console.log("A idade é igual a:" idade)  //Saída como: 18
