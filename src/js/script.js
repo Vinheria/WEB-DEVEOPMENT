aa5732f6eeb5936c0ecb52fde920cf458bbff5a4
Giovanna <giosay@hotail.com>
2025-04-18 17:49:21 -0300

    Criando variável com valor - ex.1

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,7 @@ console.log("A fruta se chama": fruta);
 //Variável com valor vazio
 let cor = null;
 console.log("A cor escolhida é": cor);
+
+//Variável com valor
+let idade = 18;
+console.log("A idade é igual a:" idade)
