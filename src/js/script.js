c1ec97159ae6e2d16acaf12fe3d055aad2dd8c9d
Giovanna <giosay@hotail.com>
2025-04-18 17:58:05 -0300

    Adicionando variáveis com seus valores, e operações

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,15 @@ console.log("A cor escolhida é": cor);  //Saída como: string vazia
 //Variável com valor
 let idade = 18;
 console.log("A idade é igual a:" idade)  //Saída como: 18
+
+
+//Exercício 2
+
+let a = 12;
+let b = "12";
+let c = 8;
+
+cconsole.log("a != c:", a != c);
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a >= c:", a >= c); 
