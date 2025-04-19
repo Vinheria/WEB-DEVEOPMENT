25c019f2b51680a9e9f264ad1a109e1f5d912d25
giovana <giovanalarocca07@gmail.com>
2025-04-18 23:38:14 -0300

    executando exercicio 3

--- src/js/script.js
+++ src/js/script.js
@@ -24,6 +24,37 @@ console.log("a == b:", a == b);         // true (valores iguais, tipos diferente
 console.log("a === b:", a === b);       // false (valores iguais, tipos diferentes)
 console.log("a >= c:", a >= c);         // true (12 é maior ou igual a 8)
 
+// EXERCÍCIO 3
+// Solicita os dados do usuário
+let peso = parseFloat(prompt("Digite seu peso em kg:"));
+let altura = parseFloat(prompt("Digite sua altura em metros:"));
+
+// Calcula o IMC
+let imc = peso / (altura * altura);
+let classificacao;
+
+// Usa switch com true para verificar a faixa de IMC
+switch (true) {
+    case (imc < 18.5):
+        classificacao = "Abaixo do peso";
+        break;
+    case (imc >= 18.5 && imc <= 24.9):
+        classificacao = "Peso ideal";
+        break;
+    case (imc > 24.9):
+        classificacao = "Acima do peso";
+        break;
+    default:
+        classificacao = "Valor inválido";
+        break;
+}
+
+// Exibe os resultados no console
+console.log(Peso: ${peso} kg);
+console.log(Altura: ${altura} m);
+console.log(IMC: ${imc.toFixed(2)});
+console.log(Classificação: ${classificacao}
+
 
 //Exercício 9
 
